879f0c62e3e47b2b036307001707cb8f91ce5192
paryee <paryee2000@gmail.com>
2024-07-18 15:37:40 +0100

    first django model project

---
+++ settings.py
@@ -0,0 +1,17 @@
+# Postgre SQL
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'paryee@12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+INSTALLED_APPS = (
+    'orm',
+)
+
+SECRET_KEY = 'SECRET KEY for this Django Project'
